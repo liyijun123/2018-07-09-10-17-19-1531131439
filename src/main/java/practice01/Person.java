0f9f3e2d40332ee3a88301228281298422ddb629
liyijun123 <52227521+liyijun123@users.noreply.github.com>
2019-08-22 22:50:27 +0800

    Update Person.java

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -1,4 +1,33 @@
 package practice01;
 
 public class Person {
+
+	private String name;
+	private int age;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public Person(String string, int i) {
+		this.name = string;
+		this.age=i;
+	}
+
+	public String introduce() {
+		// TODO Auto-generated method stub
+		return "My name is "+this.name+". I am "+this.age+" years old.";
+	}
 }
