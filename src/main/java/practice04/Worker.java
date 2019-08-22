867ce524af8565258afe6f4b6ea416a0bfa4afd5
liyijun123 <52227521+liyijun123@users.noreply.github.com>
2019-08-22 23:00:56 +0800

    Update Worker.java

--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,32 +1,5 @@
 package practice04;
 
-public class Person {
-	private String name;
-	private int age;
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
-
-	public Person(String string, int i) {
-		this.name = string;
-		this.age=i;
-	}
-
-	public String introduce() {
-		// TODO Auto-generated method stub
-		return "My name is "+this.name+". I am "+this.age+" years old.";
-	}
+public class Worker {
+	
 }
